2c106ab5c669c39fd3bb5c999de9e98991c99b38
simpsons01 <ilikekobe01@gmail.com>
2022-09-30 17:38:53 +0800

    hello world

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
 const h1 = document.createElement("h1")
-h1.textContent = "this is home page"
+h1.textContent = "this is home page, and some home text"
 const app = document.querySelector("#app")
 app.appendChild(h1)
