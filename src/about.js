a634412e982804d883d669d1ece0111052eae9c8
simpsons01 <ilikekobe01@gmail.com>
2022-10-20 14:16:00 +0800

    about page add description

--- src/about.js
+++ src/about.js
@@ -1,4 +1,7 @@
 const h1 = document.createElement("h1")
 h1.textContent = "this is about page"
+const p = document.createElement("p")
+p.textContent = "this is about page description"
 const app = document.querySelector("#app")
 app.appendChild(h1)
+app.appendChild(p)
