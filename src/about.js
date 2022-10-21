06ae5a35532e560609e0976168bace4030b7fb13
simpsons01 <ilikekobe01@gmail.com>
2022-10-21 13:58:54 +0800

    about page add description 2

--- src/about.js
+++ src/about.js
@@ -1,6 +1,8 @@
 import { createDomText } from "./utils/dom"
 const title = createDomText("h1", "this is about page")
 const description1 = createDomText("p", "this is about page description 1")
+const description2 = createDomText("p", "this is about page description 2")
 const app = document.querySelector("#app")
 app.appendChild(title)
 app.appendChild(description1)
+app.appendChild(description2)
