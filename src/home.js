a8163c8bbbac37ab894fc2c01f2d4add44c13730
simpsons01 <ilikekobe01@gmail.com>
2022-10-21 14:39:30 +0800

    add home page and about page description

--- src/home.js
+++ src/home.js
@@ -1,4 +1,6 @@
 import { createDomText } from "./utils/dom"
 const title = createDomText("h1", "this is home page")
+const description = createDomText("p", "this is home page description")
 const app = document.querySelector("#app")
 app.appendChild(title)
+app.appendChild(description)
