3c7010cfa425fa22ca7f789bc29ee860ddbec008
simpsons01 <ilikekobe01@gmail.com>
2022-10-25 14:21:41 +0800

    remove about page description

--- src/about.js
+++ src/about.js
@@ -1,6 +1,4 @@
 import { createDomText } from "./utils/dom"
 const title = createDomText("h1", "this is about page")
-const description = createDomText("p", "this is about page description")
 const app = document.querySelector("#app")
 app.appendChild(title)
-app.appendChild(description)
