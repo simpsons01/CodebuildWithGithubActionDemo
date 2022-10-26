f0ea13998764a5d3e17d34dca3486db419c55d4b
simpsons01 <ilikekobe01@gmail.com>
2022-10-26 11:57:33 +0800

    remove description1 & descripton2

--- src/about.js
+++ src/about.js
@@ -1,10 +1,6 @@
 import { createDomText } from "./utils/dom"
 const title = createDomText("h1", "this is about page")
 const description = createDomText("p", "this is about page description")
-const description2 = createDomText("p", "this is about page description2")
-const description3 = createDomText("p", "this is about page description3")
 const app = document.querySelector("#app")
 app.appendChild(title)
 app.appendChild(description)
-app.appendChild(description2)
-app.appendChild(description3)
