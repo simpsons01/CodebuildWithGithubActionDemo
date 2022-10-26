7e8a926dbc491c3d6b6e9f0dcd901292135ede62
simpsons01 <ilikekobe01@gmail.com>
2022-10-26 14:30:49 +0800

    add home page to subtitle

--- src/home.js
+++ src/home.js
@@ -1,6 +1,8 @@
 import { createDomText } from "./utils/dom"
 const title = createDomText("h1", "this is home page")
+const subTitle = createDomText("h2", "this is home page subTitle")
 const description = createDomText("p", "this is home page description")
 const app = document.querySelector("#app")
 app.appendChild(title)
 app.appendChild(description)
+app.appendChild(subTitle)
