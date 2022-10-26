a2d1de562af530af1b5335ee9fe7ee8dedd5eee6
simpsons01 <ilikekobe01@gmail.com>
2022-10-26 13:59:20 +0800

    add about page footer

--- src/about.js
+++ src/about.js
@@ -2,8 +2,10 @@ import { createDomText } from "./utils/dom"
 const title = createDomText("h1", "this is about page")
 const subTitle = createDomText("h2", "this is about page subTitle")
 const description = createDomText("p", "this is about page description")
+const footer = createDomText("p", "this is about page footer text")
 
 const app = document.querySelector("#app")
 app.appendChild(title)
 app.appendChild(subTitle)
 app.appendChild(description)
+app.appendChild(footer)
