7c6ae8dfbd4207fb8b2ded5d256d411de2a3b6af
simpsons01 <ilikekobe01@gmail.com>
2022-10-27 13:52:16 +0800

    remove description

--- src/about.js
+++ src/about.js
@@ -1,9 +1,7 @@
 import { createDomText } from "./utils/dom"
 const title = createDomText("h1", "this is about page")
 const subTitle = createDomText("h2", "this is about page subTitle")
-const description = createDomText("p", "this is about page description, and some text, and some text, and some text")
 
 const app = document.querySelector("#app")
 app.appendChild(title)
 app.appendChild(subTitle)
-app.appendChild(description)
