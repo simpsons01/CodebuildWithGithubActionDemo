0c1c0269962027cc2cf35ff7b476cd4fb18ef0cc
simpsons01 <ilikekobe01@gmail.com>
2022-11-17 00:05:38 +0800

    add description2

--- src/home.js
+++ src/home.js
@@ -1,10 +1,12 @@
 import { createDomText } from "./utils/dom"
 const title = createDomText("h1", "this is home page")
 const subTitle = createDomText("h2", "this is home page subTitle")
-const description = createDomText("p", "this is home page description")
+const description1 = createDomText("p", "this is home page description1")
+const description2 = createDomText("p", "this is home page description2")
 const footer = createDomText("p", "this is home page footer text")
 
 const app = document.querySelector("#app")
 app.appendChild(title)
-app.appendChild(description)
+app.appendChild(description1)
+app.appendChild(description2)
 app.appendChild(subTitle)
