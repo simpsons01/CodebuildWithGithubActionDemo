0a437211cecdb402184532d916fa07334d33f1da
simpsons01 <39054010+simpsons01@users.noreply.github.com>
2022-11-18 11:34:10 +0800

    aboute page add description 1 (#34)

--- src/about.js
+++ src/about.js
@@ -1,10 +1,12 @@
 import { createDomText } from "./utils/dom"
 const title = createDomText("h1", "this is about page")
 const subTitle = createDomText("h2", "this is about page subTitle")
+const description1 = createDomText("p", "this is description 1")
 const descriptionShine = createDomText("p", "this is Shine's test")
 
 
 const app = document.querySelector("#app")
 app.appendChild(title)
 app.appendChild(subTitle)
+app.appendChild(description1)
 app.appendChild(descriptionShine)
