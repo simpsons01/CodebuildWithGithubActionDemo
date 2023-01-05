e7c12aadde1b9bb84dd5715175f65b22f1954a8e
simpsosns01 <ilikekobe01@gmail.com>
2023-01-05 17:52:58 +0800

    add description2

--- src/about.js
+++ src/about.js
@@ -3,10 +3,12 @@ const title = createDomText("h1", "this is about page")
 const subTitle = createDomText("h2", "this is about page subTitle")
 const description1 = createDomText("p", "this is description 1")
 const descriptionShine = createDomText("p", "this is Shine's test")
+const description2 = createDomText("p", "this is description 2")
 
 
 const app = document.querySelector("#app")
 app.appendChild(title)
 app.appendChild(subTitle)
 app.appendChild(description1)
+app.appendChild(description2)
 app.appendChild(descriptionShine)
